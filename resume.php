f4541321a4a583cf8aa5852c9d69a232fe49d546
Ashith Raghunath <dranzerashi@users.noreply.github.com>
2015-07-05 21:17:03 +0530

    Update resume.php

--- resume.php
+++ resume.php
@@ -1,15 +1,63 @@
 <?php
 session_start();
+$uploadfail=-1;
 if(!isset($_SESSION['UID']))
 {
   header("location:placements.php");
 }
-if(isset($_POST["resume"]))
+if(isset($_POST["submit"]))
 {
 include_once "databaseconnection.php";
-$sql = 'SELECT * FROM student where UID="' . $_SESSION["UID"] . '"';
-$res = mysql_query($sql, $con);
-$row = mysql_fetch_array($res);
+    $allowedExts = array("doc", "docx", "pdf", "odf");
+    $temp = explode(".", $_FILES["resume"]["name"]);
+    $extension = end($temp);
+    if ((($_FILES["resume"]["type"] == "application/msword")
+      || ($_FILES["resume"]["type"] == "application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+      || ($_FILES["resume"]["type"] == "application/pdf")
+      || ($_FILES["resume"]["type"] == "application/odf"))
+      && ($_FILES["resume"]["size"] < 5000000)
+      && in_array($extension, $allowedExts))
+        {
+        if ($_FILES["resume"]["error"] > 0)
+          {
+  			$uploadmsg="File Error Upload Failed";
+			$uploadfail=1;
+          }
+        else 
+          {
+            $fileName=$_SESSION["UID"].".".end($temp);
+			if (file_exists("resume/" . $fileName))
+            {
+			unlink("resume/".$fileName);
+            }
+			$temp = explode(".",$_FILES["resume"]["name"]);
+			$newfilename = $_SESSION["UID"].".".end($temp);
+			if(move_uploaded_file($_FILES["resume"]["tmp_name"], "resume/" . $newfilename))
+				{
+				$sql = 'UPDATE student SET Resume="' . $newfilename . '"WHERE UID="' . $_SESSION["UID"] . '"';
+				mysql_query($sql, $con);
+				$uploadfail=0;
+				$uploadmsg="Uploaded Successfully";
+				}
+			else
+				{
+				$uploadfail=1;
+				$uploadmsg="Upload Failed";
+				}
+            
+          }
+        }
+      else
+        {
+			$uploadmsg="Upload Failed! ";
+			if($_FILES["resume"]["size"] > 5000000)
+			{
+				$uploadmsg=$uploadmsg."Max size 5MB";
+			}
+			$uploadfail=1;
+        }
+
+
 }
 
 $route='login';
@@ -18,10 +66,17 @@ require_once( 'header.php' );
 <?php if(!isset($_POST['resume'])){ ?>
 <div id="resume" class="col-md-offset-4 col-md-4">
   <h3>Upload Resume</h3>
-  <?php if($uploadfail == 1) { ?>
-  <div class="alert alert-danger alert-dismissible" role="alert">
+  <?php if($uploadfail >=0) {
+	  if($uploadfail==0){
+		  $alerttype="alert-success";
+	  }
+	  else{
+		  $alerttype="alert-danger";
+	  }
+  ?>
+  <div class="alert <?php echo $alerttype; ?> alert-dismissible" role="alert">
     <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-    Failed to upload resume. Please try again!
+    <?php echo $uploadmsg; ?>
   </div>
   <?php } ?>
   <form action="" method="post"  enctype="multipart/form-data">
@@ -29,7 +84,7 @@ require_once( 'header.php' );
       <label >Select file: </label>
       <input name="resume" class="form-control" type="file" accept=".pdf,.doc,.docx,.odf">
     </div>
-    <button type="submit" class="btn btn-default">Upload</button>
+    <input type="submit" name="submit" class="btn btn-default" value="Upload"/>
   </form>
 </div>
   <?php 
